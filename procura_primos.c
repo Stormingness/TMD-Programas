96ba3e3fd032a5ab5248f66502862e75192ddc54
Stormingness <113128611+Stormingness@users.noreply.github.com>
2025-01-14 20:51:20 +0000

    Update final de procurar_primos.c

--- procura_primos.c
+++ procura_primos.c
@@ -4,7 +4,7 @@
 #include <stdbool.h>
 #include <time.h>
 
-//#define sizeOfVector 99999
+
 
 typedef struct No{
     long long num;
@@ -22,6 +22,8 @@ NO *CriaNo(long long n){
 NO *AdicionaNo(NO *head, NO *novo){
     if(head == NULL){
         printf("\nErro na busca da cabeca da lista 'head' em AdicionaNo\n");
+    }
+    
     if(novo == NULL){
         printf("\nError na busca do elemdnto 'novo' da lista em AdicionaNo\n");
         return head;
@@ -39,107 +41,67 @@ NO *AdicionaNo(NO *head, NO *novo){
 
 
 
-bool semLema(long long num);
+void apresentaExplicacao(){
+    printf("\n\n ##### FUNCIONALIDADE DO PROGRAMA #####\n");
+    printf("\nAo iniciar o programa, vai ser pedido ao utilizador para inserir um numero.");
+    printf("\nO programa pede ao utilizador para inserir um novo numero se este for < 2.");
+    printf("\nEste numero agora passa a ser chamado o 'numero a avaliar' para simplificar a explicacao.");
+    printf("\nO numero a avaliar sera o numero desejado para verificar se ele e ou nao primo.");
+    printf("\nApos inserir o numero a avaliar, um menu vai aparecer que contem as opcoes para cada algoritmo.");
+    printf("\nDepois, o programa pede ao utilizador para inserir a opcao do menu apresentado.");
+    printf("\nO programa pede ao utilizador para inserir uma nova opcao se esta nao for valida.");
 
-bool comLema(long long num);
+    printf("\n\nSe o utilizador inserir o numero '0', vai aparecer a explicacao do programa.");
 
-//long long getPrimes(long long *primeVector, int vectorSize, long long limit);
-void getPrimes(NO *vetorPrimos, long long limit);
-
-//bool comLemaTFA(long long num, long long *primeVector, long long vectorSize, int *numPrimes, long long *factVector);
-bool comLemaTFA(long long num, NO *vetorPrimos, int *numPrimes, NO *factoresVectorPrimos);
-
-//void factoriseNum(long long num, long long *primeVector, long long vectorSize);
-void factorizarNum(long long num, NO *fatoresVetor);
-
-
-int main(){
-
-    printf("\nPrograma criada por Junior Coelho\n");
-    printf("\n#################################\n");
-
-/*
-    int sec = time;
-
-    int hour = sec / 3600;
-    sec = sec - (hour*3600);
-
-    int min = sec / 60;
-    sec = sec - (min*60);
-*/
-
-    long long num;
-    printf("\nInsira um numero: ");
-    scanf("%lld", &num);
-    //printf("\nGot: %lld\n", num);
-
-    int op;
-
-    printf("\n--- MENU ---");
-    printf("\n1. Sem Lema");
-    printf("\n2. Com Lema");
-    printf("\n3. Com Lema + TFA\n");
-
-    do{
-    printf("Insira opcao do menu: ");
-    scanf("%d", &op);
-    }while(op < 1 || op > 3);
-
-    clock_t startTime, endTime;
-    double totalTime;
-    bool isPrime;
-
-    switch(op){
-        case 1: startTime = clock();
-                isPrime = semLema(num);
-                endTime = clock();
-                break;
-
-        case 2: startTime = clock();
-                isPrime = comLema(num);
-                endTime = clock();
-                break;
-
-        case 3: //long long primes[sizeOfVector];
-                NO *ListaPrimos = CriaNo(0);
-                long long k;
-
-                //long long newVectorSize = getPrimes(primes, sizeOfVector, num);
-                //long long newVectorSize = getPrimes(ListaPrimos, num);
-                getPrimes(ListaPrimos, num);
-
-                int NumberOfPrimes;
-
-                //long long factoriseVector[sizeOfVector];
-                NO *fatoresVetorPrimos = CriaNo(0);
-
-                startTime = clock();
-                isPrime = comLemaTFA(num, ListaPrimos, &NumberOfPrimes, fatoresVetorPrimos);
-                endTime = clock();
-
-                //printf("\nNumber of factor primes: %d", NumberOfPrimes);
-
-                if(NumberOfPrimes>0){
-                    factorizarNum(num, fatoresVetorPrimos);
-                }
-
-                break;
-        
-        default: printf("\nErro na execucao da opcao %d", op);
-    }
-
-    totalTime = (double) (endTime - startTime) / CLOCKS_PER_SEC;
-
-    if(isPrime)
-        printf("\n\n--> Conclusao: %lld e numero primo", num);
-    else
-        printf("\n\n--> Conclusao: %lld nao e numero primo", num);
-
-    printf("\n\n--> Tempo total de execucao do metodo escolihdo (em segundos): %.9lf\n\n", totalTime);
-
-    system("pause");
-    return -1;
+    printf("\n\nSe o utilizador inserir o numero '1', vai executar o algoritmo 'Sem Lema'.");
+    printf("\nO tempo do CPU e registado aqui como tempo inicial.");
+    printf("\nEste algoritmo comeca com um numero i = 2.");
+    printf("\nO algoritmo verifica se existe resto da divisao entre o numero a avaliar e o numero i.");
+    printf("\nSe houver resto da divisao, entao nao se conclui que o numero a avaliar e primo ainda.");
+    printf("\nLogo que nao houver resto da divisao (ou seja resto = 0), entao conclui-se que o numero a avaliar e primo.");
+    printf("\nApos cada verificacao, o numero i incrementa um valor (+1) ate ao numero a avaliar.");
+    printf("\nO algoritmo verifica a divisao para cada incremento de i, fazendo as mesmas conclusoes.");
+    printf("\nCada vez que i divide o numero a avaliar, ele e registado no programa.");
+    printf("\nQuando o algoritmo se conclui, o tempo do CPU e registado denovo aqui como tempo final.");
+    printf("\nO tempo final e calculado como o quociente entre a variacao do tempo do CPU e 'CLOCKS_PER_SEC'.");
+    printf("\n'CLOCKS_PER_SEC' e um valor que indica quanto tempo de processamento do CPU existe num segundo.");
+    
+    printf("\n\nSe o utilizador inserir o numero '2', vai executar o algoritmo 'Com Lema'.");
+    printf("\nO tempo do CPU e registado aqui como tempo inicial.");
+    printf("\nEste algoritmo comeca com um numero i = 2.");
+    printf("\nO algoritmo verifica se existe resto da divisao entre o numero a avaliar e o numero i.");
+    printf("\nSe houver resto da divisao, entao nao se conclui que o numero a avaliar e primo ainda.");
+    printf("\nLogo que nao houver resto da divisao (ou seja resto = 0), entao conclui-se que o numero a avaliar e primo.");
+    printf("\nApos cada verificacao, o numero i incrementa um valor (+1) ate a raiz do numero a avaliar.");
+    printf("\nO algoritmo verifica a divisao para cada incremento de i, fazendo as mesmas conclusoes.");
+    printf("\nCada vez que i divide o numero a avaliar, ele e registado no programa.");
+    printf("\nQuando o algoritmo se conclui, o tempo do CPU e registado denovo aqui como tempo final.");
+    printf("\nO tempo final e calculado como o quociente entre a variacao do tempo do CPU e 'CLOCKS_PER_SEC'.");
+    printf("\n'CLOCKS_PER_SEC' e um valor que indica quanto tempo de processamento do CPU existe num segundo.");
+    
+    printf("\n\nSe o utilizador inserir o numero '3', vai executar o algoritmo 'Com Lema + TFA'.");
+    printf("\nO algoritmo inicia a calcular os numeros primos que divide o numero a avaliar ate a raiz do mesmo.");
+    printf("\nEstes numeros sao guardados numa lista (ligada) para armazenar os numeros primos.");
+    printf("\nEmbora irrelevante, o tempo do calculo dos numeros primos e igualmente medido como nos outros algoritmos.");
+    printf("\nO tempo do CPU e registado aqui como tempo inicial (para o algoritmo pretendido).");
+    printf("\nEste algoritmo comeca com um numero i = primeiro numero da lista de numeros primos.");
+    printf("\nO algoritmo verifica se existe resto da divisao entre o numero a avaliar e o numero i.");
+    printf("\nSe houver resto da divisao, entao nao se conclui que o numero a avaliar e primo ainda.");
+    printf("\nLogo que nao houver resto da divisao (ou seja resto = 0), entao conclui-se que o numero a avaliar e primo.");
+    printf("\nApos cada verificacao, o numero i muda para o seguinte numero na lista.");
+    printf("\nO algoritmo verifica a divisao para cada mudanca de i, fazendo as mesmas conclusoes.");
+    printf("\nCada vez que i divide o numero a avaliar, ele e registado no programa.");
+    printf("\nQuando o algoritmo se conclui, o tempo do CPU e registado denovo aqui como tempo final.");
+    printf("\nO tempo final e calculado como o quociente entre a variacao do tempo do CPU e 'CLOCKS_PER_SEC'.");
+    printf("\n'CLOCKS_PER_SEC' e um valor que indica quanto tempo de processamento do CPU existe num segundo.");
+    printf("\n--- BONUS ---");
+    printf("\nApos o calculo do tempo final, o programa faz a decomposicao do numero a avaliar.");
+    printf("\nEsta decomposicao mostra-se usando o Teorema Fundamental da Aritmetica.");
+    printf("\nO programa divide o numero a avaliar por cada numero primo armazenado.");
+    printf("\nPode haver varias divisoes do mesmo numero primo para calcular o seu expoente.\n\n");
 }
+
+
 
 bool semLema(long long num){
     long long i;
@@ -156,6 +118,8 @@ bool semLema(long long num){
     return isPrime;
 }
 
+
+
 bool comLema(long long num){
     long long i;
     bool isPrime = true;
@@ -171,74 +135,56 @@ bool comLema(long long num){
     return isPrime;
 }
 
-//long long getPrimes(NO *vetorPrimos, long long limit){
+
+
 void getPrimes(NO *vetorPrimos, long long limit){
 
     long long i, j, pos=0, posCheck = 0;
 
     bool i_isPrime = true;
 
-        for(i=2; i<=sqrt(limit); i++){
-            for(j=2; j<=sqrt(i); j++){
-                if((i % j) == 0){
+    NO *temp = vetorPrimos;
+
+    for(i=2; i<=sqrt(limit); i++){
+        //for(j=2; j<=sqrt(i); j++){
+        while(temp != NULL){
+            if(temp->num != 0){
+                if((i % (temp->num)) == 0){
                     //printf("\ni = %lld not prime", i);
                     i_isPrime = false;
                     break;
                 }
             }
-
-            if(i == 2){
-                vetorPrimos->num = i;
-            }
-            else{
-                if(i_isPrime){
-                    //printf("\ni = %lld is prime", i);
-
-                    NO *novoElemento = CriaNo(i);
-                    vetorPrimos = AdicionaNo(vetorPrimos, novoElemento);
-
-                    /*
-                    NO *temp = vetorPrimos;
-
-                    printf("\nPos before loop: %lld", pos);
-                    printf("\nPosCheck before loop: %lld", posCheck);
-
-                    for(posCheck = 0; posCheck < pos; posCheck++){
-                        printf("\nPosCheck in loop: %lld", posCheck);
-                        temp = temp->prox;
-                        posCheck++;
-                    }
-                    printf("\nPosCheck after loop: %lld", posCheck);
-                    //posCheck = 0;
-                    */
-
-                    //temp = novoElemento;
-
-                    //printf("\nNumero neste temp = %lld\n", temp->num);
-
-                    //primeVector[pos] = i;
-
-                    //printf("\nprimeVector[%lld] = %lld", pos, primeVector[pos]);
-                    //pos++;
-                }
-            }
-
-            i_isPrime = true;
+            temp = temp->prox;
         }
 
-        NO *temp1 = vetorPrimos;
+        if(i == 2){
+            vetorPrimos->num = i;
+        }
+        else{
+            if(i_isPrime){
+                //printf("\ni = %lld e primo", i);
 
-        //printf("\n\nLISTA PRIMOS ATE RAIZ:");
-        while(temp1 != NULL){
-            //printf("\n%lld", temp1->num);
-            temp1 = temp1->prox;
+                NO *novoElemento = CriaNo(i);
+                vetorPrimos = AdicionaNo(vetorPrimos, novoElemento);
+
+            }
         }
 
-    //return pos;
+        i_isPrime = true;
+        temp = vetorPrimos;
+    }
+
+    NO *temp1 = vetorPrimos;
+
+    //printf("\n\nLISTA PRIMOS ATE RAIZ:");
+    while(temp1 != NULL){
+        //printf("\n%lld", temp1->num);
+        temp1 = temp1->prox;
+    }
 }
 
-//bool comLemaTFA(long long num, long long *primesVector, long long vectorSize, int *numPrimes, long long *factVector){
-bool comLemaTFA(long long num, NO *vetorPrimos, int *numPrimes, NO *fatoresVetorPrimos){ 
+bool comLemaTFA(long long numAvaliar, NO *vetorPrimos, int *numPrimes, NO *fatoresVetorPrimos){ 
     long long i;
     bool isPrime = true;
 
@@ -248,8 +194,8 @@ bool comLemaTFA(long long num, NO *vetorPrimos, int *numPrimes, NO *fatoresVetor
 
     //for(i=0; i<vectorSize; i++){
     while(temp != NULL){
-        if((num % (temp->num)) == 0){
-            printf("\n%lld|%lld", temp->num, num);
+        if((numAvaliar % (temp->num)) == 0){
+            printf("\n%lld|%lld", temp->num, numAvaliar);
             isPrime = false;
 
             if(NumOfPrimes == 0){
@@ -277,14 +223,12 @@ bool comLemaTFA(long long num, NO *vetorPrimos, int *numPrimes, NO *fatoresVetor
     return isPrime;
 }
 
-//void factoriseNum(long long num, long long *primeVector, long long vectorSize){
-void factorizarNum(long long num, NO *fatoresVetor){
-    
+void factorizarNum(long long numAvaliar, NO *fatoresVetor){
+
     printf("\n");
 
-    long long number = num;
+    long long number = numAvaliar;
 
-    //long long exponentVector[vectorSize];
     NO *exponentesVetor = CriaNo(0);
 
     NO *temp = fatoresVetor;
@@ -296,19 +240,10 @@ void factorizarNum(long long num, NO *fatoresVetor){
         temp = temp->prox;
     }
 
-    /*
-    for(j=1; j<numFatores; j++){
-        exponentVector[j] = 0;
-    }
-    */
-
-    //int k;
-
     NO *temp1 = fatoresVetor;
     NO *temp2 = exponentesVetor;
 
-    //for(k=0; k<vectorSize; k++){
-    while(temp1 != NULL);
+    while(temp1 != NULL){
         while((number % (temp1->num)) == 0){
             number = number / (temp1->num);
             (temp2->num)++;
@@ -318,14 +253,11 @@ void factorizarNum(long long num, NO *fatoresVetor){
         temp2 = temp2->prox;
     }
 
-    printf("\nTeorema Fundamental da Aritmetica:\n%lld = ", num);
-
-    //int n;
+    printf("\nTeorema Fundamental da Aritmetica:\n%lld = ", numAvaliar);
 
     temp1 = fatoresVetor;
     temp2 = exponentesVetor;
 
-    //for(n=0; n<(vectorSize-1); n++){
     while(temp2->prox != NULL){
         if(temp2->num == 1){
             printf("%lld x ", temp1->num);
@@ -344,7 +276,7 @@ void factorizarNum(long long num, NO *fatoresVetor){
 
         }
         else{
-            printf("%lld^%lld x %lld", temp1-1>num, temp2->num, number);
+            printf("%lld^%lld x %lld", temp1->num, temp2->num, number);
         }
     }
     else{
@@ -354,5 +286,113 @@ void factorizarNum(long long num, NO *fatoresVetor){
         else{
             printf("%lld^%lld", temp1->num, temp2->num);
         }
+    }
+}
+
+
+
+int main(){
+
+    printf("\nPrograma criada por Junior Coelho\n");
+    printf("\n#################################\n");
+
+    long long num;
+
+    printf("\n");
+
+    do{
+    printf("Insira um numero: ");
+    scanf("%lld", &num);
+    }while(num < 2);
+
+    int op;
+
+    printf("\n--- MENU ---");
+    printf("\n0. Funcionalidade do Programa");
+    printf("\n1. Sem Lema");
+    printf("\n2. Com Lema");
+    printf("\n3. Com Lema + TFA\n");
+
+    do{
+    printf("Insira opcao do menu: ");
+    scanf("%d", &op);
+    }while(op < 0 || op > 3);
+
+    clock_t startTime, endTime;
+    double totalTime;
+    bool isPrime;
+
+    switch(op){
+        case 0: apresentaExplicacao();
+                break;
+
+        case 1: printf("\nExecutar algoritmo...\n");
+        
+                startTime = clock();
+                isPrime = semLema(num);
+                endTime = clock();
+                break;
+
+        case 2: printf("\nExecutar algoritmo...\n");
+        
+                startTime = clock();
+                isPrime = comLema(num);
+                endTime = clock();
+                break;
+
+        case 3: NO *ListaPrimos = CriaNo(0);
+                long long k;
+
+                printf("\nCalcular os numeros primos ate a raiz...\n");
+
+                startTime = clock();
+                getPrimes(ListaPrimos, num);
+                endTime = clock();
+
+                int NumberOfPrimes;
+
+                printf("\nCalculo dos numeros primos ate a raiz completo!\n");
+
+                totalTime = (double) (endTime - startTime) / CLOCKS_PER_SEC;
+
+                printf("\n--> Tempo total do calculo dos numeros primos ate a raiz (em segundos): %.9lf\n\n", totalTime);
+
+                NO *fatoresVetorPrimos = CriaNo(0);
+
+                printf("\nExecutar algoritmo...\n");
+
+                startTime = clock();
+                isPrime = comLemaTFA(num, ListaPrimos, &NumberOfPrimes, fatoresVetorPrimos);
+                endTime = clock();
+
+                //printf("\nNumero de divisores que sao primos: %d", NumberOfPrimes);
+
+                if(NumberOfPrimes>0){
+                    factorizarNum(num, fatoresVetorPrimos);
+                }
+
+                break;
+        
+        default: printf("\nErro na execucao da opcao %d", op);
+    }
+
+    if(op == 0){
+        system("pause");
+
+        return -1;
+    }
+    else{
+        totalTime = (double) (endTime - startTime) / CLOCKS_PER_SEC;
+
+        if(isPrime)
+            printf("\n\n--> Conclusao: %lld e numero primo", num);
+        else
+            printf("\n\n--> Conclusao: %lld nao e numero primo", num);
+
+        printf("\n\n--> Tempo total de execucao do metodo escolihdo (em segundos): %.9lf\n\n", totalTime);
+
+        system("pause");
+
+        return -1;
     }
 }
